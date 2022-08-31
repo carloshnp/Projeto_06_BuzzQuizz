0f1b35e72e79729a2804c017ed9e315bb4611b08
Antonio Prado <antonioeprado18@gmail.com>
2022-08-31 20:57:40 -0300

    Criado a API de local storage

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,19 @@
 const buzzAPI = {
     quizzes: "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes",
+    storeQuizz: (id, key) => {
+        // ID é um número a partir de 1, não zero!!
+        // ID tem que ser passado como string!!
+        localStorage.setItem(id, key);
+    },
+    getQuizz: (id) => {
+        // Aqui também...
+        localStorage.getItem(id);
+    },
+    numberOfKeys: localStorage.length
 }
 
-function getQuizzes() {
-    const promise = axios.get(buzzAPI.quizzes)
+function getQuizzes(id="") {
+    const promise = axios.get(`${buzzAPI.quizzes}/${id}`)
     .then((response) => {
         response.data.forEach(quizz => {
             renderQuizzes(quizz);
@@ -21,17 +31,23 @@ function getQuizzes() {
 }
 
 function renderQuizzes(obj) {
-    // Quando tivermos o armazenamento vai ser algo assim o codigo
-    // const userQuizzes = document.querySelector(".user-quizzes");
-    // for(const key in user.keys) {
-    //     if(obj.key === user.key) {
-    //         const quizz = document.createElement("article");
-    //         quizz.className = "quizz";
-    //         quizz.style.backgroundImage = `url(${obj.image})`;
-    //         quizz.innerHTML = obj.title;
-    //         userQuizzes.insertAdjacentElement("beforeend", quizz);
-    //     }
-    // }
+
+    const userQuizzes = document.querySelector(".user-quizzes");
+    const numOfQuizzes = buzzAPI.numberOfKeys;
+    if(!numOfQuizzes){
+        for(const key in numberOfKeys) {
+
+            const storedKey = buzzAPI.getQuizz(`${key}`);
+            const storedQuizz = getQuizzes(storedKey);
+            const quizz = document.createElement("article");
+            quizz.className = "quizz";
+            quizz.style.backgroundImage = `url(${storedQuizz.image})`;
+            quizz.innerHTML = storedQuizz.title;
+            userQuizzes.insertAdjacentElement("beforeend", quizz);
+
+        }
+    }
+
     const buzzQuizzes = document.querySelector(".buzz-quizzes");
     const quizz = document.createElement("article");
     quizz.className = "quizz";
@@ -42,6 +58,7 @@ function renderQuizzes(obj) {
         document.querySelector(".tela1").style.display = "none";
         document.querySelector(".tela2").style.display = "initial";
         // depois tem que mudar pra abrir o quizz especifico!!
+
     })
 }
 
@@ -52,6 +69,7 @@ function createQuizz() {
         document.querySelector(".tela3").style.display = "initial";
     })
 }
+
 
 getQuizzes();
 createQuizz();
